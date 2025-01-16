697f5226d9e35e2492bd76c5ece68b8c84daa615
Niknamenik <nikita20nik00@gmail.com>
2025-01-16 20:44:47 +0100

    homework is done

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 let hr = prompt ('How many hours?');
 let sec = hr * 60;
-alert (hr + ' hours' + ' = ' + sec + ' seconds')
+alert (hr + ' hours' + ' = ' + sec + ' seconds');
+console.log (hr + ' hours' + ' = ' + sec + ' seconds');
