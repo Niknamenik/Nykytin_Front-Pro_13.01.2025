c75cf3bcdde98f8521fbeefe4b4d4d6f8b0acea1
Niknamenik <nikita20nik00@gmail.com>
2025-02-24 21:26:40 +0100

    hw done

--- script.js
+++ script.js
@@ -1,75 +1,24 @@
-// Створити масив, довжину та елементи якого задає користувач. Потім відсортувати масив за зростанням. 
-// Потім видалити елементи з масиву з 2 по 4 (включно). У міру змін виводити вміст масиву на сторінку.
+//Дано масив об’єктів. Вивести масив телефонних номерів користувачів в яких баланс більше ніж 2000 доларів. І знайти суму всіх балансів користувачів
 
-let arr = [1, 5, 7, 10, 34, 2];
-console.log(arr);
-arr.sort((a, b) => a - b);
-console.log(arr);
-arr.splice(1, 3, );
-console.log(arr);
+let users = [{"index":0,"isActive":true,"balance":"$2,226.60","name":"Eugenia Sawyer","gender":"female","phone":"+1 (840) 583-3207","address":"949 John Street, Rose, Puerto Rico, 1857"}, 
+    {"index":1,"isActive":true,"balance":"$2,613.77","name":"Pauline Gallegos","gender":"female","phone":"+1 (985) 593-3328","address":"328 Greenpoint Avenue, Torboy, North Dakota, 6857"}, 
+    {"index":2,"isActive":false,"balance":"$3,976.41","name":"Middleton Chaney","gender":"male","phone":"+1 (995) 591-2478","address":"807 Fleet Walk, Brutus, Arkansas, 9783"}, 
+    {"index":3,"isActive":true,"balance":"$1,934.58","name":"Burns Poole","gender":"male","phone":"+1 (885) 559-3422","address":"730 Seba Avenue, Osage, Alabama, 6290"}, 
+    {"index":4,"isActive":true,"balance":"$3,261.65","name":"Mcfadden Horne","gender":"male","phone":"+1 (942) 565-3988","address":"120 Scholes Street, Kirk, Michigan, 1018"}, 
+    {"index":5,"isActive":false,"balance":"$1,790.56","name":"Suzette Lewis","gender":"female","phone":"+1 (837) 586-3283","address":"314 Dunne Place, Bawcomville, Guam, 9053"}]
 
-// Дано масив
-const arr_1 = [16,-37,54,-4,72,-56,47,4,-16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
+let usersBalance = users.filter((value) => parseFloat(value.balance.replace(/[$,]/g, "")) > 2000);
+console.log(usersBalance);
 
-// Знайти суму та кількість позитивних елементів.
-let resOfSum = arr_1.filter((element) => element > 0);
+let phoneArr = [];
 
-console.log(resOfSum.length);
-console.log(resOfSum);
+for (let i = 0; i <= usersBalance.length - 1; i ++){
+    phoneArr.push(usersBalance[i].phone); 
+};
+console.log(phoneArr);
+let summOfDep = 0;
 
-let initialValue = 0;
-let sumWithInitial = resOfSum.reduce(
-    (accumulator, currentValue) => accumulator + currentValue, initialValue);
-    console.log(sumWithInitial);
-    
-// Знайти мінімальний елемент масиву та його порядковий номер.    
-let arrInARow = arr_1.toSorted((a, b) => a - b);
-console.log(arrInARow);
-
-let min = arrInARow.shift();
-let findMinIndex = arr_1.findIndex((element) => element === min);
-
-console.log(`${min} - мінімальний елемент, його iндекс - ${findMinIndex}`);
-
-// Знайти максимальний елемент масиву та його порядковий номер.
-let max = arrInARow.pop();
-let findMaxIndex = arr_1.findIndex((element) => element === max);
-
-console.log(`${max} - максимальний елемент, його iндекс - ${findMaxIndex}`)
-
-// Визначити кількість негативних елементів.  
-
-let negativeArr = arr_1.filter((element) => element < 0);
-
-console.log(negativeArr.length);
-
-// Знайти кількість непарних позитивних елементів.
-
-const countNotParOffPositive = arr_1.filter(num => num > 0 && num % 2 !== 0);
-console.log(countNotParOffPositive.length);
-
-// Визначити кількість парних позитивних елементів.
-
-const countParOffPositive = arr_1.filter(num => num > 0 && num % 2 === 0);
-console.log(countParOffPositive.length);
-
-// Знайти суму парних позитивних елементів.
-
-let sumOfPar = countParOffPositive.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-console.log(sumOfPar);
-
-// Знайти суму непарних позитивних елементів.
-
-let sumOfNotPar = countNotParOffPositive.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-console.log(sumOfNotPar);   
-
-// Знайти добуток позитивних елементів.
-
-let mult = resOfSum.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
-console.log(mult);
-
-//  Знайти найбільший серед елементів масиву, решту занулити.
-
-const maxElement = Math.max(...arr_1);
-const newArr = arr_1.map(num =>(num === maxElement ? num : 0));
-console.log(newArr);
+for (let i = 0; i <= users.length - 1; i ++){
+    summOfDep += parseFloat(users[i].balance.replace(/[$,]/g, ""));
+};
+console.log(summOfDep);
