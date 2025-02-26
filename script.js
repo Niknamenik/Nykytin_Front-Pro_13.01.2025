6f4ba842575c30ac7978d4b8e8e87a54d18bad1d
Niknamenik <nikita20nik00@gmail.com>
2025-02-26 16:08:55 +0100

    done

--- script.js
+++ script.js
@@ -1,24 +1,10 @@
-//Дано масив об’єктів. Вивести масив телефонних номерів користувачів в яких баланс більше ніж 2000 доларів. І знайти суму всіх балансів користувачів
+// Реалізуйте функцію removeElement(array, item), щоб видалити елемент item з масиву array.
 
-let users = [{"index":0,"isActive":true,"balance":"$2,226.60","name":"Eugenia Sawyer","gender":"female","phone":"+1 (840) 583-3207","address":"949 John Street, Rose, Puerto Rico, 1857"}, 
-    {"index":1,"isActive":true,"balance":"$2,613.77","name":"Pauline Gallegos","gender":"female","phone":"+1 (985) 593-3328","address":"328 Greenpoint Avenue, Torboy, North Dakota, 6857"}, 
-    {"index":2,"isActive":false,"balance":"$3,976.41","name":"Middleton Chaney","gender":"male","phone":"+1 (995) 591-2478","address":"807 Fleet Walk, Brutus, Arkansas, 9783"}, 
-    {"index":3,"isActive":true,"balance":"$1,934.58","name":"Burns Poole","gender":"male","phone":"+1 (885) 559-3422","address":"730 Seba Avenue, Osage, Alabama, 6290"}, 
-    {"index":4,"isActive":true,"balance":"$3,261.65","name":"Mcfadden Horne","gender":"male","phone":"+1 (942) 565-3988","address":"120 Scholes Street, Kirk, Michigan, 1018"}, 
-    {"index":5,"isActive":false,"balance":"$1,790.56","name":"Suzette Lewis","gender":"female","phone":"+1 (837) 586-3283","address":"314 Dunne Place, Bawcomville, Guam, 9053"}]
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-let usersBalance = users.filter((value) => parseFloat(value.balance.replace(/[$,]/g, "")) > 2000);
-console.log(usersBalance);
-
-let phoneArr = [];
-
-for (let i = 0; i <= usersBalance.length - 1; i ++){
-    phoneArr.push(usersBalance[i].phone); 
+function removeElement(array, item) {
+   const findItem =  array.indexOf(item);
+    array.splice(findItem, 1);
+    return array;
 };
-console.log(phoneArr);
-let summOfDep = 0;
-
-for (let i = 0; i <= users.length - 1; i ++){
-    summOfDep += parseFloat(users[i].balance.replace(/[$,]/g, ""));
-};
-console.log(summOfDep);
+console.log(removeElement(array, 3));
