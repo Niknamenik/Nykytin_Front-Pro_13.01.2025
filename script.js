984dd3953e95282a0abc60d8731891b7496ef1b2
Niknamenik <nikita20nik00@gmail.com>
2025-03-13 00:26:31 +0100

    bonus dz done

--- script.js
+++ script.js
@@ -1,34 +1,13 @@
-// Вам потрібно написати функцію, яка як параметр приймає функцію і додає їй можливість кешувати дзвінки.
-// Ідея полягає в тому, що при виклику функції з однаковими аргументами немає сенсу викликати функцію щоразу, достатньо зберігати дані про результати виклику.
-// Зберігати потрібно останні 10 дзвінків.
-
-function makeCall(number) {
-    console.log(`Виклик функції для: ${number}`);
-    return number;
-}
-
-function callNumbers(fn) {
-    let cache = new Map();
-
-    return function (number) {
-        if (cache.has(number)) {
-            return cache.get(number)
-        }
-
-        const result = fn(number)
-        cache.set(number, result)
-
-        if (cache.size > 10) {
-            const firstKey = cache.keys().next().value
-            cache.delete(firstKey)
-        }
-
-        console.log(`Останні 10: ${Array.from(cache.keys())}`)
+function sumNumbers(a, b) {
+    return a + b;
+  }
+  function powerSumFunction (fn, action = ''){
+      return function (...arguments){    
+        console.log(`Я вмію ${action} числа`);
+        const result = fn(...arguments); 
+        console.log(`Результат виклику функції:${result}`)
         return result
-    };
-}
-
-const cachedNumbers = callNumbers(makeCall);
-console.log(cachedNumbers(945694609));
-console.log(cachedNumbers(945694609));
-console.log(cachedNumbers(123456789));
+    }
+  }
+  const poweredSumFunc = powerSumFunction(sumNumbers, 'додавати');
+  poweredSumFunc(5, 5)
