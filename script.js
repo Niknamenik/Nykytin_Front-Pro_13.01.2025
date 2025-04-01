8453eab6f54025d77d88f25427c2d68eb082d964
Niknamenik <nikita20nik00@gmail.com>
2025-04-02 00:51:47 +0200

    started

--- script.js
+++ script.js
@@ -1,37 +1,25 @@
-// Реалізувати подобу інтернет-магазину
-// Дано 3 блоки
-// У лівій частині сторінки – перелік категорій.
-// При натисканні на категорію виводиться у середній блок список товарів цієї категорії.
-// Клік на товар – інформацію про товар у правому блоці.
-// В інформації товару - кнопка "купити"
-// При натисканні на “купити” з'являється повідомлення, що товар куплено та повернення у вихідний стан програми (коли відображається лише список категорій)
+const form = document.getElementById('formData')
+const submit = document.getElementById('submit')
+let i = 0
+form.addEventListener('submit', handleSubmit)
 
-const categorysArr = Array(... document.getElementsByClassName('category')[0].querySelectorAll('li'));
-
-categorysArr.forEach((element) => element.addEventListener('click', showProducts))
-
-function showProducts (){
-    const ul = document.getElementsByClassName(`${this.innerHTML}_products`)[0]
-    const ulArr = Array(... document.getElementsByClassName('products')[0].querySelectorAll('ul'))
-    ulArr.forEach((element) => element.style = 'display:none')
-    ul.style = 'display:block'
-    const liArr = Array(... ul.getElementsByTagName('li'))
-    liArr.forEach((element) => element.addEventListener('click', showInfo))
-    const info = document.getElementsByClassName('info')[0]
-    info.innerHTML = ''
-
+function handleSubmit (event){
+    form.style = 'display:none'
+    const formData = new FormData(form)
+    event.preventDefault();
+    const test = [...formData.keys()]
+    console.log(test)
+    for(let key of formData.keys()){
+        console.log(`${key} : ${formData.get(`${key}`)}`)
+    }
+    createATable(formData);
 }
-function showInfo (){
-    const info = document.getElementsByClassName('info')[0]
-    const p = document.createElement('p')
-    const btn = document.createElement('button')
-    const li = this
-    info.style = 'display: block;'
-    info.innerHTML = ''
-    info.appendChild(p).textContent = `info about ${this.innerHTML}`
-    info.appendChild(btn).textContent = 'Buy'
-    btn.addEventListener('click', function () {
-        alert(`Done! Ty for your order (${li.textContent})`)
-        window.location.reload()
-    })
+
+function createATable (formData){
+    if(!document.body.querySelector('table')){
+        const table = document.createElement('table');
+        document.body.appendChild(table)
+        const td = document.createElement('td')
+        table.appendChild(td)
+    }
 }
