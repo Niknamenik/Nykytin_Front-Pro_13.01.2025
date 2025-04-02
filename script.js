886afd3020e2b770728cc5ec71d5340ea99fdf40
Niknamenik <nikita20nik00@gmail.com>
2025-04-02 15:17:06 +0200

    done

--- script.js
+++ script.js
@@ -1,25 +1,43 @@
 const form = document.getElementById('formData')
-const submit = document.getElementById('submit')
-let i = 0
 form.addEventListener('submit', handleSubmit)
 
 function handleSubmit (event){
     form.style = 'display:none'
     const formData = new FormData(form)
     event.preventDefault();
-    const test = [...formData.keys()]
-    console.log(test)
-    for(let key of formData.keys()){
-        console.log(`${key} : ${formData.get(`${key}`)}`)
-    }
     createATable(formData);
 }
 
 function createATable (formData){
+
     if(!document.body.querySelector('table')){
         const table = document.createElement('table');
         document.body.appendChild(table)
-        const td = document.createElement('td')
-        table.appendChild(td)
     }
+    const keysArr = [...formData.keys()]
+
+    for(let i = 0; i < keysArr.length; i++){
+        const newTr = document.createElement('tr')
+        const firstTd = document.createElement('td')
+        const table = document.querySelector('table')
+        const secondTd = document.createElement('td')
+
+        if(keysArr[i].slice(0, 8) !== 'language'){
+            table.appendChild(newTr)
+            newTr.appendChild(firstTd).innerHTML = keysArr[i]
+            newTr.appendChild(secondTd).innerHTML = formData.get(`${keysArr[i]}`)
+        }
+        else{
+            if(document.querySelectorAll('td')[document.querySelectorAll('td').length - 2].innerHTML !== 'languages'){
+                table.appendChild(newTr)
+                newTr.appendChild(firstTd).innerHTML = `${keysArr[i].slice(0, 8)}s`
+                newTr.appendChild(secondTd).innerHTML = formData.get(`${keysArr[i]}`)
+            }
+            const lastTd = document.querySelectorAll('td')[document.querySelectorAll('td').length - 1]
+            if(formData.get(`${keysArr[i + 1]}`) !== null){
+            lastTd.innerHTML = lastTd.innerHTML + `, ${formData.get(`${keysArr[i + 1]}`)}`
+            }
+        }
+    }
+    
 }
