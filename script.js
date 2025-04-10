22017de9f2250da3feb205fd1a01039bcb3f6f34
Niknamenik <nikita20nik00@gmail.com>
2025-04-10 23:48:11 +0200

    done

--- script.js
+++ script.js
@@ -1,103 +1,43 @@
-const formAddUser = document.querySelector('#add_user_form')
-const formEditUser = document.querySelector('#edit_user_form')
-const addUserInput = document.querySelector('#add_user')
-const editUserInput = document.querySelector('#edit_user')
-const tbody = document.querySelector('tbody')
-const editBtn = document.querySelector('#edit_user_btn')
 
-window.addEventListener('load', buildATable)
 
-function buildATable(){
-    const users = JSON.parse(localStorage.getItem('users'))
-    if(users){
-        users.forEach((user) => buildARow(user.name, user.id));
+function CreateAPerson (name, age){
+    if(!name || age < 18){  
+        alert("Некоректні дані: ім'я обов'язкове, вік має бути ≥ 18");
     }
+    this.name = name;
+    this.age = age;
 
+    this.showInfo = function () {
+        console.log(`Name:${name}, Age:${age}`);
+    };
 }
 
-function buildARow (name, id){
-    const tr =  document.createElement('tr')
-    const td = document.createElement('td')
-    tr.setAttribute('data_id', id)
-    td.textContent = name || "Unknown"
+function CreateACar (brand, year, color){
+    this.brand = brand;
+    this.year = year;
+    this.color = color;
+    this.owner = person;
 
-    tr.append(td)
-    tr.append(createButtons('View', 'view'))
-    tr.append(createButtons('Edit', 'edit'))
-    tr.append(createButtons('Remove', 'remove'))
-
-    tbody.append(tr)
-}
-
-function createButtons (btnName, btnId){
-    const td = document.createElement('td')
-    const button = document.createElement('button')
-    button.setAttribute('id', btnId)
-    button.textContent = btnName
-    td.append(button)
-    return td
-}
-
-formAddUser.addEventListener('submit', (event) => {
-    event.preventDefault()
-
-    const name = addUserInput.value
-    const id = + new Date()
-
-    buildARow(name, id)
-
-    const usersFromLS = JSON.parse(localStorage.getItem('users'))
-    if(usersFromLS){
-        usersFromLS.push({name:name || "Unknown", id: id})
-        localStorage.setItem('users', JSON.stringify(usersFromLS))
-    }else{
-        localStorage.setItem('users', JSON.stringify([{name:name || "Unknown", id:id}]))}
-
-    formAddUser.reset();
-})
-
-tbody.addEventListener('click', (event) => {
-    const button = event.target.getAttribute('id')
-    const tr = event.target.closest('tr');
-    const trId = tr.getAttribute('data_id')
-
-    switch(button){
-        case 'view':
-            console.log(tr.querySelector("td").textContent, trId)
-            break;
-        case 'edit':
-            editUserInput.removeAttribute('disabled')
-            editBtn.removeAttribute('disabled')
-            editUserInput.value = tr.querySelector('td').textContent
-            editUserInput.focus()
-
-            localStorage.setItem('id_to_edit', trId)
-            break;
-        case 'remove':
-            const usersFromLS = JSON.parse(localStorage.getItem('users'))
-            const newUseresToLS = usersFromLS.filter((element) => element.id !== Number(trId))
-            localStorage.setItem('users', JSON.stringify(newUseresToLS))
-            tr.remove()
-            break;
-            default:
-                break;
+    this.showInfo = function (){
+        console.log(`Car:${this.brand}, Year:${this.year}, Color:${this.color}`);
+        if(this.owner){
+            console.log(`Owner: ${this.owner.name}, Age: ${this.owner.age}`);
+        } else {
+            console.log('This car has no owner');
+        };
     }
-})
+}
 
-formEditUser.addEventListener('submit', (event) => {
-    event.preventDefault()
+const personName = prompt("Введіть ім’я:");
+const age = parseInt(prompt("Введіть вік:"));
 
-    const editedName = editUserInput.value
-    const id = +localStorage.getItem('id_to_edit')
+const person = new CreateAPerson(personName, age);
 
-    document.querySelector(`tr[data_id="${id}"] td`).textContent = editedName
+person.showInfo();
 
-    const usersFromLS = JSON.parse(localStorage.getItem('users'))
-    const indexOfUserToFix = usersFromLS.indexOf(usersFromLS.find((element) => element.id === id))
-    usersFromLS[indexOfUserToFix].name = editedName
-    localStorage.setItem('users', JSON.stringify(usersFromLS))
+const brand = prompt("Марка авто:");
+const color = prompt("Колiр авто:");
+const year = parseInt(prompt("Рік випуску:"));
 
-    editUserInput.disabled = true
-    editBtn.disabled = true
-    formEditUser.reset();
-})
+const car = new CreateACar(brand, color, year);
+car.showInfo();
