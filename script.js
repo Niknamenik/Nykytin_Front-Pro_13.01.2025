dbddd9b39919eb20fe173f474804e1a124e012c3
Niknamenik <nikita20nik00@gmail.com>
2025-04-14 19:34:57 +0200

    done

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 
 function CreateAPerson (name, age){
     if(!name || age < 18){  
-        alert("Некоректні дані: ім'я обов'язкове, вік має бути ≥ 18");
     }
     this.name = name;
     this.age = age;
@@ -12,26 +11,38 @@ function CreateAPerson (name, age){
     };
 }
 
-function CreateACar (brand, year, color){
+function CreateACar (brand, year, color, owner){
     this.brand = brand;
     this.year = year;
     this.color = color;
-    this.owner = person;
+    this.owner = owner;
 
     this.showInfo = function (){
-        console.log(`Car:${this.brand}, Year:${this.year}, Color:${this.color}`);
-        if(this.owner){
-            console.log(`Owner: ${this.owner.name}, Age: ${this.owner.age}`);
-        } else {
-            console.log('This car has no owner');
-        };
+        console.log(`Car:${this.brand}, Year:${this.year}, Color:${this.color}, Owner:${this.owner.name}`);
     }
 }
 
-const personName = prompt("Введіть ім’я:");
-const age = parseInt(prompt("Введіть вік:"));
+const personName = getPersonName ()
+const personAge = getPersonAge ()
 
-const person = new CreateAPerson(personName, age);
+function getPersonName (){
+    const personName = prompt("Введіть ім’я:");
+    if(!personName){
+        alert("Некоректні дані: ім'я обов'язкове");
+        return getPersonName ()
+    }
+    return personName;
+}
+function getPersonAge (){
+    const age = parseInt(prompt("Введіть вік:"));
+    if(!age || age < 18){
+        alert("Некоректні дані: Вік має бути ≥ 18");
+        return getPersonAge ()
+    }
+    return age;
+}
+
+const person = new CreateAPerson(personName, personAge);
 
 person.showInfo();
 
@@ -39,5 +50,5 @@ const brand = prompt("Марка авто:");
 const color = prompt("Колiр авто:");
 const year = parseInt(prompt("Рік випуску:"));
 
-const car = new CreateACar(brand, color, year);
+const car = new CreateACar(brand, color, year, person);
 car.showInfo();
