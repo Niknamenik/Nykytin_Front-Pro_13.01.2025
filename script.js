ba6ad180f6303eb3615f99b88350adf789291ac0
Niknamenik <nikita20nik00@gmail.com>
2025-04-14 21:09:04 +0200

    done

--- script.js
+++ script.js
@@ -1,54 +1,41 @@
+function CreateHTMLElement (tagName, attributes = {}, content = '') {
+    this.tagName = tagName,
+    this.atributes = attributes,
+    this.element = document.createElement(`${tagName}`),
+    this.content = content,
+    
+    this.setAttributes = function (){
+        for(let kej in attributes){
+            this.element.setAttribute(kej, attributes[kej])
+        }
+    },
 
+    this.setContent = function (){
+        if (typeof this.content === 'string') {
+            this.element.textContent = this.content;
+        } else if (this.content instanceof HTMLElement) {
+            this.element.appendChild(this.content);
+        } else if (Array.isArray(this.content)) {
+            this.content.forEach(child => {
+                if (child instanceof HTMLElement) {
+                    this.element.appendChild(child);
+                }
+            });
+        }
+    },
 
-function CreateAPerson (name, age){
-    if(!name || age < 18){  
+    this.render = function (){  
+        document.body.appendChild(this.element)
+    },
+
+    this.getElement = function(){
+        return this.element
     }
-    this.name = name;
-    this.age = age;
 
-    this.showInfo = function () {
-        console.log(`Name:${name}, Age:${age}`);
-    };
+    this.setAttributes()
+    this.setContent()
+    this.render()
 }
 
-function CreateACar (brand, year, color, owner){
-    this.brand = brand;
-    this.year = year;
-    this.color = color;
-    this.owner = owner;
-
-    this.showInfo = function (){
-        console.log(`Car:${this.brand}, Year:${this.year}, Color:${this.color}, Owner:${this.owner.name}`);
-    }
-}
-
-const personName = getPersonName ()
-const personAge = getPersonAge ()
-
-function getPersonName (){
-    const personName = prompt("Введіть ім’я:");
-    if(!personName){
-        alert("Некоректні дані: ім'я обов'язкове");
-        return getPersonName ()
-    }
-    return personName;
-}
-function getPersonAge (){
-    const age = parseInt(prompt("Введіть вік:"));
-    if(!age || age < 18){
-        alert("Некоректні дані: Вік має бути ≥ 18");
-        return getPersonAge ()
-    }
-    return age;
-}
-
-const person = new CreateAPerson(personName, personAge);
-
-person.showInfo();
-
-const brand = prompt("Марка авто:");
-const color = prompt("Колiр авто:");
-const year = parseInt(prompt("Рік випуску:"));
-
-const car = new CreateACar(brand, color, year, person);
-car.showInfo();
+// EXEMPLE
+// const button = new CreateHTMLElement('button',{class:'hiiden',id:"btn"},'My button')
