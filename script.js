419826a52a5e095c7501d58aaea19f9558108393
Niknamenik <nikita20nik00@gmail.com>
2025-04-16 23:51:53 +0200

    hw is done

--- script.js
+++ script.js
@@ -1,41 +1,63 @@
-function CreateHTMLElement (tagName, attributes = {}, content = '') {
-    this.tagName = tagName,
-    this.atributes = attributes,
-    this.element = document.createElement(`${tagName}`),
-    this.content = content,
-    
-    this.setAttributes = function (){
-        for(let kej in attributes){
-            this.element.setAttribute(kej, attributes[kej])
-        }
-    },
+const houseForm = document.querySelector("#house_creator");
+const residentsInput = document.querySelector("#residents");
+const flatsInput = document.querySelector("#flats");
+const flatsForm = document.querySelector("#flat-form");
 
-    this.setContent = function (){
-        if (typeof this.content === 'string') {
-            this.element.textContent = this.content;
-        } else if (this.content instanceof HTMLElement) {
-            this.element.appendChild(this.content);
-        } else if (Array.isArray(this.content)) {
-            this.content.forEach(child => {
-                if (child instanceof HTMLElement) {
-                    this.element.appendChild(child);
-                }
-            });
-        }
-    },
+houseForm.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-    this.render = function (){  
-        document.body.appendChild(this.element)
-    },
+  const numOfResidents = residentsInput.value;
+  const numOfFlats = flatsInput.value;
 
-    this.getElement = function(){
-        return this.element
+  document.body.appendChild(flatsForm);
+  const button = document.createElement("button");
+  button.setAttribute("type", "submit");
+  button.setAttribute("id", "save-btn");
+  button.innerText = "Зберегти мешканців";
+
+  for (let i = 1; i <= numOfFlats; i++) {
+    const div = document.createElement(`div`);
+    div.setAttribute("class", "flat");
+    div.setAttribute("id", `flat_No-${i}`);
+    flatsForm.appendChild(div).innerHTML = `<p>Kвартирa ${i}</p>`;
+
+    for (let n = 1; n <= numOfResidents; n++) {
+      const label = document.createElement("label");
+      div.append(label);
+      label.innerHTML = `<input type='text' required data-resident='${n}' data-flat='${i}' placeholder='Мешканець ${n}'>`;
     }
+    flatsForm.prepend(button);
+    flatsForm.style = "border: 1px solid";
+  }
+});
 
-    this.setAttributes()
-    this.setContent()
-    this.render()
+flatsForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const houseArr = [];
+  const flats = document.getElementsByClassName("flat");
+  const showBtn = document.querySelector("#show-btn");
+  for (let i = 0; i < flats.length; i++) {
+    const flatInputs = document.querySelectorAll(`#flat_No-${i + 1} input`);
+    let flatArr = [];
+    houseArr.push(flatArr);
+    flatInputs.forEach((input) => {
+      flatArr.push(input.value);
+    });
+  }
+  localStorage.setItem("house", JSON.stringify(houseArr));
+  showBtn.removeAttribute("class");
+  showBtn.addEventListener("click", showTheHouse);
+  alert("Мешканців збережено!");
+});
+
+function showTheHouse() {
+  const div = document.createElement("div");
+  const houseArr = JSON.parse(localStorage.getItem("house"));
+
+  document.body.append(div);
+  houseArr.forEach((flat, index) => {
+    const paragraf = document.createElement("p");
+    div.appendChild(paragraf).innerText = `Kвартирa ${index + 1}: ${flat}`;
+  });
 }
-
-// EXEMPLE
-// const button = new CreateHTMLElement('button',{class:'hiiden',id:"btn"},'My button')
