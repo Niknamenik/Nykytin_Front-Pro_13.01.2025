9773aa41094e9d912834c8d04c7e8bad5186ac10
Niknamenik <nikita20nik00@gmail.com>
2025-04-17 16:29:48 +0200

    started

--- script.js
+++ script.js
@@ -1,69 +1,32 @@
-const houseForm = document.querySelector("#house_creator");
-const residentsInput = document.querySelector("#residents");
-const flatsInput = document.querySelector("#flats");
-const flatsForm = document.querySelector("#flat-form");
+// static small_size = { price: 50, calories: 20 };
+// static big_size = { price: 100, calories: 40 };
+// static stuffing_cheese = { price: 10, calories: 20 };
+// static stuffing_salad = { price: 20, calories: 5 };
+// static stuffing_potato = { price: 5, calories: 10 };
+// static topping_mayo = { price: 20, calories: 10 };
+// static topping_sauce = { price: 15, calories: 0 };
 
-houseForm.addEventListener("submit", (event) => {
-  event.preventDefault();
+// static sizeOptions = [small_size, big_size];
+// static stuffingOptions = [stuffing_cheese, stuffing_salad, stuffing_potato];
+// static toppingOptions = [topping_sauce, topping_mayo];
+class Humburger {
+  static small_size = { price: 50, calories: 20 };
+  static big_size = { price: 100, calories: 40 };
+  static stuffing_cheese = { price: 10, calories: 20 };
+  static stuffing_salad = { price: 20, calories: 5 };
+  static stuffing_potato = { price: 5, calories: 10 };
+  static topping_mayo = { price: 20, calories: 10 };
+  static topping_sauce = { price: 15, calories: 0 };
 
-  const numOfResidents = residentsInput.value;
-  const numOfFlats = flatsInput.value;
-  const button = document.createElement("button");
-
-  button.setAttribute("type", "submit");
-  button.setAttribute("id", "save-btn");
-  button.innerText = "Зберегти мешканців";
-  const flats = document.querySelectorAll(".flat");
-  flats.forEach((flat) => flat.remove());
-  document.querySelector("#show-btn").setAttribute("class", "hidden");
-
-  for (let i = 1; i <= numOfFlats; i++) {
-    const div = document.createElement(`div`);
-    div.setAttribute("class", "flat");
-    div.setAttribute("id", `flat_No-${i}`);
-    flatsForm.appendChild(div).innerHTML = `<p>Kвартирa ${i}</p>`;
-
-    for (let n = 1; n <= numOfResidents; n++) {
-      const label = document.createElement("label");
-      div.append(label);
-      label.innerHTML = `<input type='text' required data-resident='${n}' data-flat='${i}' placeholder='Мешканець ${n}'>`;
-    }
-    if (!document.querySelector("#save-btn")) {
-      flatsForm.prepend(button);
-    }
-    flatsForm.style = "border: 1px solid";
-    houseForm.reset();
+  constructor(size, stuffings = {}, toppings = {}) {
+    this.size = size;
+    this.stuffings = stuffings;
+    this.toppings = toppings;
   }
-});
-
-flatsForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  const houseArr = [];
-  const flats = document.getElementsByClassName("flat");
-  const showBtn = document.querySelector("#show-btn");
-  for (let i = 0; i < flats.length; i++) {
-    const flatInputs = document.querySelectorAll(`#flat_No-${i + 1} input`);
-    let flatArr = [];
-    houseArr.push(flatArr);
-    flatInputs.forEach((input) => {
-      flatArr.push(input.value);
-    });
-  }
-  localStorage.setItem("house", JSON.stringify(houseArr));
-  showBtn.removeAttribute("class");
-  showBtn.addEventListener("click", showTheHouse);
-  alert("Мешканців збережено!");
-});
-
-function showTheHouse() {
-  const div = document.createElement("div");
-  const houseArr = JSON.parse(localStorage.getItem("house"));
-
-  document.body.append(div);
-  div.setAttribute("class", "house");
-  houseArr.forEach((flat, index) => {
-    const paragraf = document.createElement("p");
-    div.appendChild(paragraf).innerText = `Kвартирa ${index + 1}: ${flat}`;
-  });
 }
+const humburger = new Humburger(
+  Humburger.small_size,
+  Humburger.stuffing_cheese,
+  Humburger.topping_sauce
+);
+console.log(humburger);
