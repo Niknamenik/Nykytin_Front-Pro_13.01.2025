be2ab4406f4070e66f103398b51c8ac732ecba31
Niknamenik <nikita20nik00@gmail.com>
2025-04-17 19:41:02 +0200

    update

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-// static small_size = { price: 50, calories: 20 };
-// static big_size = { price: 100, calories: 40 };
-// static stuffing_cheese = { price: 10, calories: 20 };
-// static stuffing_salad = { price: 20, calories: 5 };
-// static stuffing_potato = { price: 5, calories: 10 };
-// static topping_mayo = { price: 20, calories: 10 };
-// static topping_sauce = { price: 15, calories: 0 };
-
-// static sizeOptions = [small_size, big_size];
-// static stuffingOptions = [stuffing_cheese, stuffing_salad, stuffing_potato];
-// static toppingOptions = [topping_sauce, topping_mayo];
 class Humburger {
   static small_size = { price: 50, calories: 20 };
   static big_size = { price: 100, calories: 40 };
