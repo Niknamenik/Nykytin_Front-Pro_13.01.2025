5f1004db5c9d7d06aecde43ed9fb0c671ad012ac
Niknamenik <nikita20nik00@gmail.com>
2025-04-17 17:09:50 +0200

    done

--- script.js
+++ script.js
@@ -15,18 +15,69 @@ class Humburger {
   static stuffing_cheese = { price: 10, calories: 20 };
   static stuffing_salad = { price: 20, calories: 5 };
   static stuffing_potato = { price: 5, calories: 10 };
-  static topping_mayo = { price: 20, calories: 10 };
+  static topping_mayo = { price: 20, calories: 5 };
   static topping_sauce = { price: 15, calories: 0 };
 
-  constructor(size, stuffings = {}, toppings = {}) {
+  constructor(size, stuffing = [], topping = []) {
     this.size = size;
-    this.stuffings = stuffings;
-    this.toppings = toppings;
+    this.stuffing = [stuffing];
+    this.topping = [topping];
+  }
+
+  calculatePrice() {
+    let price =
+      this.size.price +
+      this.stuffing.reduce(
+        (accumulator, currentValue) => accumulator + currentValue.price,
+        0
+      ) +
+      this.topping.reduce(
+        (accumulator, currentValue) => accumulator + currentValue.price,
+        0
+      );
+    return price;
+  }
+
+  calculateCalories() {
+    let calories =
+      this.size.calories +
+      this.stuffing.reduce(
+        (accumulator, currentValue) => accumulator + currentValue.calories,
+        0
+      ) +
+      this.topping.reduce(
+        (accumulator, currentValue) => accumulator + currentValue.calories,
+        0
+      );
+    return calories;
+  }
+
+  addStuffing(stuffing) {
+    this.stuffing.push(stuffing);
+  }
+
+  addTopping(topic) {
+    this.topping.push(topic);
   }
 }
+
+//Example
 const humburger = new Humburger(
   Humburger.small_size,
   Humburger.stuffing_cheese,
   Humburger.topping_sauce
 );
-console.log(humburger);
+console.log("Calories:" + humburger.calculateCalories());
+console.log("Price:" + humburger.calculatePrice());
+
+console.log("Add some mayo");
+humburger.addTopping(Humburger.topping_mayo);
+
+console.log("Calories:" + humburger.calculateCalories());
+console.log("Price:" + humburger.calculatePrice());
+
+console.log("Add some potato");
+humburger.addStuffing(Humburger.stuffing_potato);
+
+console.log("Calories:" + humburger.calculateCalories());
+console.log("Price:" + humburger.calculatePrice());
