f83f1c50f036c8fcbe1e83393453b9b1faa5ca53
Niknamenik <nikita20nik00@gmail.com>
2025-04-22 18:08:44 +0200

    done

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
-const conteiner = document.querySelector(".timer_conteiner");
+const slider = document.getElementsByClassName("slider")[0];
+const btnNext = document.getElementById("next_btn");
+const btnPrev = document.getElementById("prev_btn");
+let timerId = setInterval(slideNext, 3000);
 
-function setTimer() {
-  const date = new Date();
-  const hours = date.getHours().toString().padStart(2, 0);
-  const minutes = date.getMinutes().toString().padStart(2, 0);
-  const secunds = date.getSeconds().toString().padStart(2, 0);
-  conteiner.innerHTML = `<img src="./imgs/numbers 1-10/${hours[0]}.png" alt="" />
-  <img src="./imgs/numbers 1-10/${hours[1]}.png" alt="" />
-  <hr>
-  <img src="./imgs/numbers 1-10/${minutes[0]}.png" alt="" />
-  <img src="./imgs/numbers 1-10/${minutes[1]}.png" alt="" />
-  <hr>
-  <img src="./imgs/numbers 1-10/${secunds[0]}.png" alt="" />
-  <img src="./imgs/numbers 1-10/${secunds[1]}.png" alt="" />`;
+let offset = 0;
+
+slider.style.left = offset;
+btnNext.addEventListener("click", function () {
+  if (offset === -3600) {
+    offset = 0;
+    slider.style.left = offset + "px";
+  } else {
+    offset -= 400;
+    slider.style.left = offset + "px";
+  }
+  resetInterval();
+});
+btnPrev.addEventListener("click", function () {
+  if (offset === 0) {
+    offset = -3600;
+    slider.style.left = offset + "px";
+  } else {
+    offset += 400;
+    slider.style.left = offset + "px";
+  }
+  resetInterval();
+});
+
+function slideNext() {
+  if (offset === -3600) {
+    offset = 0;
+    slider.style.left = offset + "px";
+  } else {
+    offset -= 400;
+    slider.style.left = offset + "px";
+  }
 }
 
-setInterval(setTimer, 1000);
+function resetInterval() {
+  clearInterval(timerId);
+  timerId = setInterval(slideNext, 3000);
+}
