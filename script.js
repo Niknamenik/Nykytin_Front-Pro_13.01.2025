b9367a5eae8b5d8223fbc71b1cd2c2f4f05fadbb
Niknamenik <nikita20nik00@gmail.com>
2025-04-24 15:49:23 +0200

    done

--- script.js
+++ script.js
@@ -8,49 +8,49 @@ const responses = [
   "Я трохи втомився...",
   "Можливо варто на цьому зупинитись?",
   "Дякую за розмову :)",
-  "Я йду, бувай!",
+  "Що саме ти маєш на увазi?",
 ];
 
 const chatBox = document.getElementById("chatBox");
 const input = document.getElementById("userInput");
+const button = document.getElementById("sendBtn");
 let isChatActive = true;
 
-function appendMessage(author, text) {
+function appendMessage(author, message) {
   const p = document.createElement("p");
-  p.innerHTML = `<strong>${author}:</strong> ${text}`;
-  chatBox.appendChild(p);
-  chatBox.scrollTop = chatBox.scrollHeight;
+  p.innerHTML = `<strong>${author}:</strong> ${message}`;
+  chatBox.append(p);
 }
 
 function sendMessage() {
-  if (!isChatActive) return;
+  button.disabled = true;
+  input.disabled = true;
+  if (!isChatActive) {
+    return;
+  }
   const message = input.value.trim();
-  if (!message) return;
+  if (!message) {
+    return;
+  }
   appendMessage("Ви", message);
   input.value = "";
 
-  if (message.toLowerCase() === "my watch has ended") {
-    appendMessage("Браузер", "Зрозуміло. Гарного дня!");
-    isChatActive = false;
-    return;
-  }
-
   const delay = Math.floor(Math.random() * 10000) + 1000;
-  setTimeout(() => {
-    if (!isChatActive) return;
 
-    // 10% ймовірність завершити діалог браузером
-    if (Math.random() < 0.1) {
-      appendMessage("Браузер", "Мені треба йти, до зустрічі!");
+  setTimeout(() => {
+    if (message.toLowerCase() === "my watch has ended") {
+      appendMessage("Браузер", "Зрозуміло. Гарного дня!");
       isChatActive = false;
       return;
     }
-
+    if (Math.random() < 0.1) {
+      appendMessage("Браузер", "Вибач, але мені треба йти, до зустрічі!");
+      isChatActive = false;
+      return;
+    }
     const response = responses[Math.floor(Math.random() * responses.length)];
     appendMessage("Браузер", response);
+    button.disabled = false;
+    input.disabled = false;
   }, delay);
 }
-
-input.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") sendMessage();
-});
