ca95a9dc350afeca4aebf68d25d25e3e01d07fc8
Niknamenik <nikita20nik00@gmail.com>
2025-06-03 10:52:02 +0200

    update

--- PhoneBook/src/components/Table/TableItem/Button/index.jsx
+++ PhoneBook/src/components/Table/TableItem/Button/index.jsx
@@ -6,8 +6,10 @@ function Button({ id, title }) {
   function deleteItem() {
     const usersFromLS = JSON.parse(localStorage.getItem("users"));
     const newUsers = usersFromLS.filter((user) => user.id !== id);
+    const currentUser = usersFromLS.filter((user) => user.id === id)[0];
     setUsers(newUsers);
     localStorage.setItem("users", JSON.stringify(newUsers));
+    alert(`User with name: ${currentUser.name} got deleted`);
   }
   return <button onClick={deleteItem}>{title}</button>;
 }
